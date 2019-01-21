0d0651ba60446bcd929116e781e81e77e7166813
copperstick6 <copperstick6@gmail.com>
2019-01-21 16:35:00 +0800

    added packages separated some files out added reactstrap

--- src/AdminPanel.js
+++ src/AdminPanel.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, {Component} from 'react'
 import './AdminPanel.css'
+import DateSetter from './DateSetter.js'
+import { form, FormGroup, FormControl, Button } from 'react-bootstrap';
 /* eslint-enable no-unused-vars */
 
 export default class AdminPanel extends Component {
@@ -8,36 +10,30 @@ export default class AdminPanel extends Component {
 		super(props)
 		this.state = {
 			date: "",
-			time: "",
+			time: "20:00",
+			validated: false
+
 		}
-		this.handleEventCreation = this.handleEventCreation.bind(this)
+		this.saveEventDateTime = this.saveEventDateTime.bind(this)
 	}
 	componentDidMount(){
 		document.body.className = "bg-dinner"
 	}
-	handleEventCreation(event){
-		console.log(this.state.date)
-		event.preventDefault()
-		console.log("submit")
+	saveEventDateTime(date, time){
+		console.log(date)
+		console.log(time)
+		this.setState({date: date, time: time}, ()=>{
+			console.log(this.state.date)
+			console.log(this.state.time)
+		})
 	}
+
   render () {
     return (
 
         <div className="dinner-root">
 			<div className="dinner-box">
-				<div className="dinner-panel">
-					<h1>Admin Panel</h1>
-					<h2>Create a new event</h2>
-					<form onSubmit={this.handleEventCreation}>
-						<h3>Event Date: </h3>
-						<input type="date" value={this.state.date} onChange = {(event) => this.setState({date: event.target.value})}/>
-						<h3>Event Time:</h3>
-						<input type="time" value={this.state.time} onChange = {(event) => this.setState({time: event.target.value})}/>
-						<br />
-						<br />
-						<input type="submit" value="Next"/>
-					</form>
-				</div>
+				<DateSetter saveEventDateTime={this.saveEventDateTime}/>
 			</div>
         </div>
     )
